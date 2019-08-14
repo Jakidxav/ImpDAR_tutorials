efbc14803ae50ad8464df958ac97664de61466e7
Jakidxav <joshua.driscol@gmail.com>
2019-08-14 14:40:02 -0700

    adding notebook which will demonstrate how to sort contours

---
+++ autopicker/autopicker_methods.py
@@ -0,0 +1,207 @@
+import os
+import netCDF4 as nc
+import numpy as np
+import pandas as pd
+
+import copy
+
+import scipy.io as io
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+from matplotlib import colors, colorbar
+
+from skimage import measure
+from skimage.filters import gaussian
+
+from impdar import *
+
+
+
+"""
+Find the end points of a given contour (min and max x points), and then find every x point in between those two points.
+"""
+def find_contour_points(contour, argx_list):
+    #first try to save argx list
+    argx1 = np.argmin(contour[:, 1])
+    argx2 = np.argmax(contour[:, 1])
+    argx_list.append([argx1, argx2])
+    
+    #find uniquex values for each contour
+    uniquex = (list(set([int(x) for x in contour[:, 1]])))
+    
+    #get rid of x values that we don't have contour data for
+    saved_points = []
+    delete = []
+
+    for x in uniquex:
+        wherex = np.where(contour[:, 1] == x)[0]
+        if len(wherex) == 0 or len(wherex) == 1:
+            delete.append(x)
+        else:
+            if len(wherex) > 2:
+                saved_points.append(wherex[1:3])
+            else:
+                saved_points.append(wherex)
+    
+    #delete values that we don't have enough data for
+    uniquex = [x for x in uniquex if x not in delete]
+    
+    return uniquex, saved_points
+
+
+
+"""
+Find ridge structure for all contours.
+"""
+def find_ridge_points(contour, uniquex, saved_points, data, ridge_points):
+    for i, point in enumerate(saved_points):
+        if(len(point == 2)):
+            idx1 = point[0]
+            idx2 = point[1]
+        elif(len(point > 2)):
+            idx1 = point[1]
+            idx2 = point[2]
+        else:
+            raise ValueException("You need to have at least two matching y-values.")
+
+        x = uniquex[i]
+
+        y1 = int(contour[idx1, 0])
+        y2 = int(contour[idx2, 0])
+
+        if len(data[y2:y1, x]) == 0:
+            ridge_points.append(np.where(data[:, x] == np.max(data[y1:y2, x]))[0])
+        else:
+            ridge_points.append(np.where(data[:, x] == np.max(data[y2:y1, x]))[0])
+        
+    return [elem[0] for elem in ridge_points]
+
+
+
+"""
+Find peaks/troughs for a single contour
+"""
+def find_troughs_single(uniquex, ridge_points, data):
+    maxrows, maxcols = np.shape(data)
+    
+    top, bottom = [], []
+    temptop_idx, tempbottom_idx = [], []
+    temptop_points, tempbottom_points = [], []
+
+    for i, point in enumerate(uniquex):
+        row = ridge_points[i][0]
+        col = point
+        
+        #if the point holds a positive value, then we want to
+        #find the troughs on either side of it
+        if data[row, col] > 0:
+            #look for top contour
+            for j in range(row, maxrows):
+                #enter the trough
+                if (data[j, col] <= 0):
+                    temptop_idx.append(j)
+                    temptop_points.append(data[j, col])
+
+                    #enterting this if statement would signify leaving the trough
+                    if data[j+1, col] > 0:
+                        #find the minimum in that column of the trough
+                        trough_min = np.argmin(temptop_points)
+                        top.append(temptop_idx[trough_min])
+
+                        #reset these to empty for the next column
+                        temptop_idx, temptop_points = [], []
+                        break
+
+            #look for bottom contour
+            for k in range(0, row):
+                #enter the trough
+                if (data[row-k, col] <= 0):
+                    tempbottom_idx.append(row-k)
+                    tempbottom_points.append(data[row-k, col])
+
+                    #enterting this if statement would signify leaving the trough
+                    if data[row-k-1, col] > 0:
+                        #find the minimum in that column of the trough
+                        trough_bottom_min = np.argmin(tempbottom_points)
+                        bottom.append(tempbottom_idx[trough_bottom_min])
+
+                        #reset these to empty for the next column
+                        tempbottom_idx, tempbottom_points = [], []
+                        break
+
+
+        #if the point holds a negative value, then we want to
+        #find the peaks on either side of it
+        else:
+            pass
+
+    return top, bottom
+
+
+
+"""
+Find peaks/troughs for every contour.
+"""
+def find_troughs(uniquex, ridge_points, data):
+    maxrows, maxcols = np.shape(data)
+    
+    top, bottom = [], []
+    temptop_idx, tempbottom_idx = [], []
+    temptop_points, tempbottom_points = [], []
+
+    for i, point in enumerate(uniquex):
+        row = ridge_points[i]
+        col = point
+
+        #if the point holds a positive value, then we want to
+        #find the troughs on either side of it
+        if data[row, col] > 0:
+            #look for top contour
+            for j in range(row, maxrows):
+                #enter the trough
+                if (data[j, col] <= 0):
+                    #print(j)
+                    temptop_idx.append(j)
+                    temptop_points.append(data[j, col])
+
+                    #enterting this if statement would signify leaving the trough
+                    if data[j+1, col] > 0:
+                        #find the minimum in that column of the trough
+                        trough_min = np.argmin(temptop_points)
+                        top.append(temptop_idx[trough_min])
+
+                        #reset these to empty for the next column
+                        temptop_idx, temptop_points = [], []
+                        break
+
+            #look for bottom contour
+            for k in range(0, row):
+                #enter the trough
+                if (data[row-k, col] <= 0):
+                    tempbottom_idx.append(row-k)
+                    tempbottom_points.append(data[row-k, col])
+
+                    #enterting this if statement would signify leaving the trough
+                    if data[row-k-1, col] > 0:
+                        #find the minimum in that column of the trough
+                        trough_bottom_min = np.argmin(tempbottom_points)
+                        bottom.append(tempbottom_idx[trough_bottom_min])
+
+                        #reset these to empty for the next column
+                        tempbottom_idx, tempbottom_points = [], []
+                        break
+
+
+        #if the point holds a negative value, then we want to
+        #find the peaks on either side of it
+        else:
+            pass
+            #### NEED TO IMPLEMENT THE SAME THING FOR NEGATIVE CONTOURS #####
+
+    return top, bottom
+
+
+
+
+
---
+++ autopicker/create_internal_reflectors_image.py
@@ -0,0 +1,136 @@
+import os
+import netCDF4 as nc
+import numpy as np
+import pandas as pd
+
+import copy
+
+import scipy.io as io
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+from matplotlib import colors, colorbar
+
+from skimage import measure
+from skimage.filters import gaussian
+
+from impdar import *
+
+from autopicker_methods import *
+
+
+#files on disk
+green_image = '../images/nline5.png'
+green_mat = '../data/nline5_migtk_cropped.mat'
+
+#min and max for colorbar plotting
+mini = -8.774235248565674
+maxi = 9.667154026031533
+
+#load in data
+dat = io.loadmat(green_mat)
+data = dat['data']
+
+
+#get positive and negative contours
+pos = measure.find_contours(data, 3)
+neg = measure.find_contours(data, -3)
+
+#save contours and their lengths to lists
+c_pos, c_neg, c_length_pos, c_length_neg = [], [], [], []
+
+
+#positive contours
+for contour in pos:
+    c_pos.append(contour)
+
+#negative contours
+for contour in neg:
+    c_neg.append(contour)
+
+#now extract long contours
+#this could be done instead of filtering?
+for contour in pos:
+    c_length_pos.append(len(contour))
+    
+for contour in neg:
+    c_length_neg.append(len(contour))
+
+
+#create an iterable list of the contours that are longer than some arbitrary value
+long_contours_pos_idx = list(np.where(np.array(c_length_pos) > 250)[0])
+long_contours_neg_idx = list(np.where(np.array(c_length_neg) > 250)[0])
+
+
+long_contours_pos = [pos[idx] for idx in long_contours_pos_idx]
+long_contours_neg = [neg[idx] for idx in long_contours_neg_idx]
+
+#isolate some internal reflectors
+long_contours_subset = long_contours_pos[22:100]
+
+
+#save lists
+argx_list = []
+uniquex_list = []
+saved_points_list = []
+
+ridge_points_list = []
+top, bottom = [], []
+top_list, bottom_list = [], []
+
+
+for i, contour in enumerate(long_contours_subset):
+    #get shape of data
+    maxrows, maxcols = np.shape(data)
+    
+    #find x values for contour and where along that contour that x value occurs (aka, y values)
+    uniquex, saved_points = find_contour_points(contour, argx_list)
+    uniquex_list.append(uniquex)
+    saved_points_list.append(saved_points)
+        
+    #find y values and ridge points for each contour
+    ridge_points = []
+    ridge = find_ridge_points(contour, uniquex, saved_points, data, ridge_points)
+    ridge_points_list.append(ridge)
+    
+    #find top and bottom troughs/peaks
+    top, bottom = find_troughs(uniquex, ridge, data)
+    top_list.append(top)
+    bottom_list.append(bottom)
+
+
+fig, ax = plt.subplots(figsize=(12, 8))
+
+#plot grid
+p = ax.imshow(data, cmap='gray', aspect='auto', vmin=mini, vmax=maxi, interpolation='nearest')
+
+for i, contour in enumerate(long_contours_subset):
+    #plot contours
+    ax.plot(contour[:, 1], contour[:, 0], color='orange', linewidth=1)
+
+    #plot ridge points
+    ridge = ridge_points_list[i]
+    uniqx = uniquex_list[i]
+    top_ = top_list[i]
+    bottom_ = bottom_list[i]
+    
+    for j, x in enumerate(uniqx):
+        t = top_[j]
+        r = ridge[j]
+        b = bottom_[j]
+        
+        ax.scatter(x, t, color='lime', marker='o', s=10)
+        ax.scatter(x, r, color='magenta', marker='o', s=10)
+        ax.scatter(x, b, color='lime', marker='o', s=10)
+        
+    print('Done processing contour {}/{}'.format(i+1, len(long_contours_subset)))
+
+
+ax.set_ylim(3000, 500)
+
+plt.savefig('internal_reflectors1.pdf', format='pdf', bbox_inches='tight')
+
+
+
+
+
